21625a764e5e02ca1feda36d014371b2ac90faf8
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-05-16 11:04:50 +0800

    Update: input event with key code

--- src/modules/TarFn/index.ts
+++ src/modules/TarFn/index.ts
@@ -3,13 +3,26 @@ export default (node: string, ...args:Function[]) => {
   childNodes?.forEach((childNode) => {
     if (childNode instanceof HTMLElement ) {
       childNode.getAttributeNames().forEach((attr) => {
-        if (attr.indexOf('$') != -1) {
+        if (attr.slice(0, 1) === '$') {
           childNode.addEventListener(attr.slice(1), (e) => {
             args.forEach((fn) => {
               if (childNode.getAttribute(attr) === fn.name)
                 fn(e)
             })
           })
+          if (childNode.tagName === 'INPUT' && attr.indexOf('.')) {
+            const event = attr.split('.')[0]
+            const keyCode = attr.split('.')[1]
+            childNode.addEventListener(event.slice(1), (e) => {
+              const keyboardEvent = e as KeyboardEvent
+              if (keyCode === keyboardEvent.code.toLocaleLowerCase()) {
+                args.forEach((fn) => {
+                  if (childNode.getAttribute(attr) === fn.name) 
+                    fn(e)
+                })
+              }
+            })
+          }
         }
       })
     }
