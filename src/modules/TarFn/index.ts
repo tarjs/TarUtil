90a19f937b4f309e56a21d95e57a9c8dac337465
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-05-17 15:12:46 +0800

    Fix: fn bind

--- src/modules/TarFn/index.ts
+++ src/modules/TarFn/index.ts
@@ -1,32 +1,47 @@
-export default (node: string | HTMLElement, fns: any) => {
+import domNode from "../TarReactivity/domNode.type"
+
+export default (node: domNode, fns: any) => {
   const dom = typeof(node) === 'string' ? document.querySelector(node!) : node
   const childNodes = dom?.childNodes as NodeListOf<HTMLElement>
-  childNodes?.forEach((childNode) => {
-    if (childNode instanceof HTMLElement ) {
-      childNode.getAttributeNames().forEach((attr) => {
-        if (attr.slice(0, 1) === '$') {
-          childNode.addEventListener(attr.slice(1), (e) => {
-            for (const key in fns) {
-              if (childNode.getAttribute(attr) === key)
-                fns[key](e)
-            }
-          })
-          // input kkeyboard event
-          if (childNode.tagName === 'INPUT' && attr.indexOf('.')) {
-            const event = attr.split('.')[0]
-            const keyCode = attr.split('.')[1]
-            childNode.addEventListener(event.slice(1), (e) => {
-              const keyboardEvent = e as KeyboardEvent
-              if (keyCode === keyboardEvent.code.toLocaleLowerCase()) {
-                for (const key in fns) {
-                  if (childNode.getAttribute(attr) === key)
-                    fns[key](e)
-                }
+  forEachDom(childNodes, fns)
+}
+
+const forEachDom = (childNodes: NodeListOf<HTMLElement>, fns: any) => {
+  childNodes?.forEach(childNode => {
+    if (childNode instanceof HTMLElement && childNode.children.length === 0) {
+      if (childNode instanceof HTMLElement) {
+        childNode.getAttributeNames().forEach((attr) => {
+          if (attr.slice(0, 1) === '$') {
+            childNode.addEventListener(attr.slice(1), (e) => {
+              for (const key in fns) {
+                if (childNode.getAttribute(attr) === key)
+                  fns[key](e)
               }
             })
+            // input kkeyboard event
+            if (childNode.tagName === 'INPUT' && attr.indexOf('.')) {
+              const event = attr.split('.')[0]
+              const keyCode = attr.split('.')[1]
+              childNode.addEventListener(event.slice(1), (e) => {
+                const keyboardEvent = e as KeyboardEvent
+                if (keyCode === keyboardEvent.code.toLocaleLowerCase()) {
+                  for (const key in fns) {
+                    if (childNode.getAttribute(attr) === key)
+                      fns[key](e)
+                  }
+                }
+              })
+            }
           }
-        }
-      })
+        })
+      }
+    } else if (childNode instanceof HTMLElement) {
+      const itemDom = childNode.children as unknown as Array<HTMLElement>
+      const items: Array<unknown> = []
+      for (let i = 0; i < itemDom.length; i++) {
+        items.push(itemDom[i])
+      }
+      forEachDom((items as unknown as NodeListOf<HTMLElement>), fns)
     }
   })
 }
