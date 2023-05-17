7ca68dab55ca49387dd286bc7f893f61a9a2ff4e
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-05-17 14:25:17 +0800

    Add: createTimer

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import TarRequest from "./modules/TarRequest/index"
 import Observer from "./modules/TarReactivity/index"
 import TarFn from "./modules/TarFn/index"
+import __createTimer from "./modules/TarTimer/index"
 
 const tar = {
     createRequest: TarRequest,
     createObserver: Observer,
-    createFn: TarFn
+    createFn: TarFn,
+    createTimer: __createTimer
 }
 export default tar
 
 export const createRequest = TarRequest
 export const createObserver = Observer
 export const createFn = TarFn
+export const createTimer = __createTimer
---
+++ src/modules/TarTimer/index.ts
@@ -0,0 +1,23 @@
+export default class {
+  fn!: () => void
+  time!: number
+  timer!: any
+  setTimer(fn: () => void, time: number) {
+    this.time = time
+    this.fn = fn
+  }
+  startTimer() {
+    if (this.timer) {
+      window.clearTimeout(this.timer)
+      this.timer = null
+    }
+    this.timer = setTimeout(() => {
+      this.fn()
+      this.startTimer()
+    }, this.time)
+  }
+  stopTimer() {
+    window.clearTimeout(this.timer)
+    this.timer = null
+  }
+}
